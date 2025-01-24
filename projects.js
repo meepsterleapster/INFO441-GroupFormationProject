d093c99c849819e565ceb8562302126feee5cb06
Jihan Yang <jihany@uw.edu>
2025-01-23 22:47:23 -0800

    Search

---
+++ projects.js
@@ -0,0 +1,15 @@
+function searchProjects() {
+    const searchInput = document.getElementById('projectSearchInput').value.toLowerCase();
+    const projects = document.querySelectorAll('.project');
+
+    projects.forEach(project => {
+        const projectName = project.dataset.name.toLowerCase();
+        const projectMembers = project.dataset.member.toLowerCase();
+        if (projectName.includes(searchInput) || projectMembers.includes(searchInput)) {
+            project.classList.remove('hidden');
+        } else {
+            project.classList.add('hidden');
+        }
+    });
+}
+//1
---
+++ students.js
@@ -0,0 +1,21 @@
+function searchStudents() {
+    const searchInput = document.getElementById('studentSearchInput').value.toLowerCase();
+    const students = document.querySelectorAll('.card'); 
+
+    students.forEach(student => {
+        const studentName = student.dataset.name.toLowerCase();
+        const studentRole = student.dataset.role.toLowerCase();
+        const studentEmail = student.dataset.email.toLowerCase();
+
+        const isMatch = studentName.includes(searchInput) || 
+                        studentRole.includes(searchInput) || 
+                        studentEmail.includes(searchInput);
+
+        if (isMatch) {
+            student.classList.remove('hidden');
+        } else {
+            student.classList.add('hidden');
+        }
+    });
+}
+//1

8fe2a1aa95ef58aa414c057db1b5c410f325196a
JinChuan Wu <jinchuanwu@JinChuans-MacBook-Pro.local>
2025-02-04 16:50:33 -0800

    students display

--- src/component/App.js
+++ src/component/App.js
@@ -1,28 +1,50 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../index.css'; 
 import {HeaderBar} from './GenerateHeader.js';
 import { StudentPool } from './Index.js';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { getDatabase, onValue, ref} from 'firebase/database';
 import { ProjectsPanel } from "./ProjectsPanel.js";
 import { CreateProject } from "./CreateProject";
 import { UploadProfile } from './Profile.js';
 import { MyProjects } from './MyProjects.js';
 
-import INITIAL_DATA from '../data/studentdata.json';
+// import INITIAL_DATA from '../data/studentdata.json';
 import PRO_DATA from '../data/projectdata.json';
 
 export default function App() {
     const [currentInput, setcurrentInput] = useState('');
+    const [allStudents, setAllStudents] = useState([]);
 
     const filterStudents = (SearchInfo) =>{
         setcurrentInput(SearchInfo);
     }
 
-    const filteredStudents = INITIAL_DATA.filter(student => 
+    const filteredStudents = allStudents.filter(student => 
         student.name.toLowerCase().includes(currentInput.toLowerCase()) ||
         student.roles.some(role => role.toLowerCase().includes(currentInput.toLowerCase())) ||
         student.email.toLowerCase().includes(currentInput.toLowerCase())
     );
+
+    useEffect(() => {
+        const db = getDatabase();
+        const studentRef = ref(db, 'Students');
+    
+        onValue(studentRef, (snapshot) => {
+          const allstudentObj = snapshot.val();
+          if (allstudentObj) {
+            const keyArray = Object.keys(allstudentObj);
+            const allstudentArray = keyArray.map((key) => {
+              const transformed = allstudentObj[key];
+              transformed.firebaseKey = key;
+              return transformed;
+            });
+            setAllStudents(allstudentArray);
+          } else {
+            setAllStudents([]); 
+          }
+        });
+      }, []);
 
     return (
         <>
